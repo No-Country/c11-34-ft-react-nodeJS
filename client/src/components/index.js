ededd5630413cab421f357fdc15db1fd0949b87e
lucasBruma <lucasbrumatti99@gmail.com>
2023-05-22 14:37:53 -0300

    feat: cree el home. Agregue navbar, searchbar y la galeria. Falta funcionalidad y detalles

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,4 +1,7 @@
 export * from './BackgroundAuth';
 export * from './LoginForm';
 export * from './RegisterForm';
+export * from './NavBar';
+export * from './SearchBar';
+export * from './Gallery';
 export * from './index';
