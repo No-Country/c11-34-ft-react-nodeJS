ced2cfcb9f1e3f9ccf0d97fe1fcfe2e555742b1f
StephanyCS1 <stephanycastrosalas@gmail.com>
2023-05-22 16:37:08 -0500

    cards restaurants

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,25 @@
-import { Route, Routes } from "react-router-dom"
-import { Home, Login, Register } from "./pages"
-import { AuthLayout } from "./layouts"
-import { Toaster } from "react-hot-toast"
+import { Route, Routes } from "react-router-dom";
+import { Home, Login, Register } from "./pages";
+import Card from "../src/components/prueba";
+import { AuthLayout } from "./layouts";
+
+import { Toaster } from "react-hot-toast";
 
 function App() {
   return (
     <>
-        <Routes>
-            <Route path="/" element={<Home />}/>
-            <Route path="/auth" element={<AuthLayout/>}>
-                <Route index element={<Login />} />
-                <Route path="register" element={<Register />} />
-            </Route>
-        </Routes>
-        <Toaster 
-            position="top-right"
-            reverseOrder={false}
-        />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route index element={<Login />} />
+          <Route path="register" element={<Register />} />
+        </Route>
+        {/* Ruta de prueba */}
+        <Route path="/card" element={<Card />} />
+      </Routes>
+      <Toaster position="top-right" reverseOrder={false} />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
