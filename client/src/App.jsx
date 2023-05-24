306c7f3b769b85dd17fc9f819ea20b0cbf7a44ca
Matias <matiasc.02.2003@gmail.com>
2023-05-24 18:34:21 -0300

    feat : Tastes view to correct folders

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,8 @@
 import { Route, Routes } from "react-router-dom";
-import { Home, Login, Register } from "./pages";
-import CategorySelection from "./components/tastes/tasteView.jsx";
+import { Home, Login, Register, Tastes } from "./pages";
 import { AuthLayout } from "./layouts";
-
 import { Toaster } from "react-hot-toast";
+
 
 function App() {
   return (
@@ -14,7 +13,7 @@ function App() {
           <Route index element={<Login />} />
           <Route path="register" element={<Register />} />
         </Route>
-        <Route path="/tastes" element={<CategorySelection />} />
+        <Route path="/tastes" element={<Tastes />} />
       </Routes>
       <Toaster position="top-right" reverseOrder={false} />
     </>
--- client/src/pages/index.js
+++ client/src/pages/index.js
@@ -1,4 +1,5 @@
 export * from './Home';
 export * from './Login';
 export * from './Register';
-
+export * from './Tastes';
+export * from './index';
