3d7e4c5450f0d289dfe1d20c4b33adbc261f9582
Matias <matiasc.02.2003@gmail.com>
2023-05-26 02:01:11 -0300

    feat: transition delay to NavBarMobile

--- client/src/components/NavBarMobile.jsx
+++ client/src/components/NavBarMobile.jsx
@@ -1,8 +1,8 @@
 
-export function NavBarMobile() {
+export function NavBarMobile({isOpen}) {
 
   return (
-    <div className="fixed top-0 left-0 w-full h-screen bg-bg-color z-10 flex flex-col justify-between items-center py-24 px-10">
+    <div className={`${isOpen ? 'top-0' : '-top-full'} fixed  left-0 w-full h-screen bg-bg-color z-10 flex flex-col justify-between items-center py-24 px-10 transition-all duration-[400ms]`}>
         <div className="flex flex-col gap-8 justify-between items-start w-full">
             <a>Inicio</a>
             <a>Acerca de</a>
--- client/src/components/tastes/CardTastes.jsx
+++ client/src/components/tastes/CardTastes.jsx
@@ -1,14 +1,14 @@
 export function CardTastes({ name, img}) {
     return (
-        <div className="flex flex-col w-52 justify-center items-center my-5 mx-auto hover:transition duration-200 hover:scale-110 hover:border-b-2 hover:border-gray-400">
+        <div className="flex flex-col w-60   justify-center items-center mx-auto hover:transition duration-200 hover:scale-105 hover:border-b-2 hover:border-gray-400">
             <div className="w-full h-full">
                 <img
-                    className="rounded-lg object-cover w-52 h-96"
+                    className="rounded-lg object-cover w-60 h-[25rem] "
                     src={img} alt={name}
                 />
             </div>
             <div className="flex justify-center w-52 ">
-                <h3 className="text-3xl text-black-500 font-medium">
+                <h3 className="text-3xl text-black-500 font-medium font-montserrat">
                     {name}
                 </h3>
             </div>
