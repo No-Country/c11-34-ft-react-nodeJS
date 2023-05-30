da612f41c8e0018dd78e51021754a897fe3a088c
Matias <matiasc.02.2003@gmail.com>
2023-05-30 16:37:22 -0300

    feat : query search navigate to result page

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -2,11 +2,11 @@ import { GalleryRow } from "../components";
 import { galleryCards } from '../utils';
 
 
-export function Gallery({restos}) {
+export function Gallery() {
 
     return (
           <div className="flex flex-col gap-10">
-            <GalleryRow title={'Cerca de vos'} cards={restos}/>
+            <GalleryRow title={'Cerca de vos'} cards={galleryCards}/>
             <GalleryRow title={'Mejor calificados'} cards={galleryCards}/>
             <GalleryRow title={'Recomendaciones'} cards={galleryCards}/>
           </div>
--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -4,19 +4,30 @@ import calendar from '../assets/calendar.svg';
 import clock from '../assets/clock.svg';
 import user from '../assets/user.svg';
 import filter from '../assets/filter.svg';
+import { useNavigate } from 'react-router-dom';
+import { useState } from 'react';
 
-export function SearchBar({value, setValue, setRestos, restosInitial}) {
+export function SearchBar() {
+
+  const navigate = useNavigate()
+
+  const [search, setSearh] = useState('')
 
   const handleChange = (e) => {
-    setValue(e.target.value)
-    const filteredRestos = restosInitial.filter(resto => resto.title.toLowerCase().includes(e.target.value.toLowerCase()))
-    setRestos(filteredRestos)
+    setSearh(e.target.value)
   }
 
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    navigate(`/result?search=${search}`)
+  }
+
+
+
   return (
-    <div className="w-full gap-2.5 flex flex-col lg:items-center my-5 lg:flex-row lg:border-2 lg:border-border-color rounded-full lg:py-3.5 lg:px-5 lg:w-4/5 mx-auto">
+    <form onSubmit={handleSubmit} className="w-full gap-2.5 flex flex-col lg:items-center my-5 lg:flex-row lg:border-2 lg:border-border-color rounded-full lg:py-3.5 lg:px-5 lg:w-4/5 mx-auto">
         <div className="relative flex items-center py-4 rounded-full border border-border-color lg:border-0 lg:grow">
-            <input value={value} onChange={handleChange} className="flex items-center pl-9 gap-10 relative w-full text-sm text-subtitle lg:w-auto lg:grow outline-0" placeholder="Buscar por ubicacion, restaurante" />
+            <input value={search} onChange={handleChange} className="flex items-center pl-9 gap-10 relative w-full text-sm text-subtitle lg:w-auto lg:grow outline-0" placeholder="Buscar por ubicacion, restaurante" />
             <img src={magnifyingGlass} alt="magnifying glass" width={20} height={20} className='absolute left-2 lg:left-1'/>
             <img src={filter} alt="filter" width={20} height={20} className='absolute right-2 w-6 h-6 lg:hidden'/> 
         </div>
@@ -26,7 +37,7 @@ export function SearchBar({value, setValue, setRestos, restosInitial}) {
           <Selector textDefault={'2'} image={user} type='people'/>
         </div>
 
-        <button className="hidden lg:block bg-black-light text-white px-6 p-2.5 rounded-full w-full lg:w-auto font-inter text-sm">Buscar</button>
-    </div>
+        <button type='submit' className="hidden lg:block bg-black-light text-white px-6 p-2.5 rounded-full w-full lg:w-auto font-inter text-sm">Buscar</button>
+    </form>
   )
 }
